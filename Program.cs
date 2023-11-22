4b9de68e1906a35c3ed6592ae9829fb92a819c70
Nagarjun B P <118178858+NagarjunPrabhu1907@users.noreply.github.com>
2023-11-22 16:37:01 +0530

    LINQ

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        var evenNumbers = from num in numbers
+                          where num % 2 == 0
+                          select num;
+
+        Console.WriteLine("Even Numbers:");
+        foreach (var evenNumber in evenNumbers)
+        {
+            Console.WriteLine(evenNumber);
+        }
+    }
+}
