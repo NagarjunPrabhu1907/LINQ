80d85c1a7dd99927122adb2867178ef6149cc41b
Nagarjun B P <118178858+NagarjunPrabhu1907@users.noreply.github.com>
2023-11-23 17:05:03 +0530

    practised LINQ query

--- Program.cs
+++ Program.cs
@@ -17,5 +17,46 @@ class Program
         {
             Console.WriteLine(evenNumber);
         }
+
+
+
+        string paragraph = "susie           works in a shoeshine shop. Where she shines she sits, And where she sits she shines";
+
+        //IEnumerable<string> splitted = paragraph.Split(' ').AsEnumerable();
+
+        IEnumerable<string> splitted = paragraph.Split(' ', StringSplitOptions.RemoveEmptyEntries).AsEnumerable();
+        Console.WriteLine(splitted);
+
+        var test = splitted.Where(a => a.StartsWith("a", StringComparison.OrdinalIgnoreCase));
+
+        foreach (var item in splitted)
+        {
+            //if (item.StartsWith("a", StringComparison.OrdinalIgnoreCase))
+               Console.WriteLine($"{item}");
+
+        }
+
+
+
+
+        var wordsStartingWithS = from word in paragraph.Split(new[] { ' ', '.', ',', '!' }, StringSplitOptions.RemoveEmptyEntries)
+                                 where word.Length > 0 && word[0] == 's' || word[0] == 'S'
+                                 select word;
+        int wordCount = paragraph.Split(new[] { ' ', '.', ',', '!' }, StringSplitOptions.RemoveEmptyEntries).Length;
+
+        //int wordCount = paragraph.Split(new[] { "", ".", ",", "," }).Length;
+
+
+        Console.WriteLine("Words starting with 's':");
+        foreach (var word in wordsStartingWithS)
+        {
+            Console.Write(word + " ");
+        }
+
+        Console.WriteLine("\n\nTotal number of words in the paragraph: " + wordCount);
+
     }
 }
+
+
+
